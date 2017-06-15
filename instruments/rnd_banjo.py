7a174228fd6fe204443ff3c9122c4692221089d0
Guillaume Riou <guiloua2@hotmail.com>
2017-06-14 22:25:22 -0400

    Its late

--- instruments/rnd_banjo.py
+++ instruments/rnd_banjo.py
@@ -7,17 +7,17 @@ from random import choice
 s = Server(audio="jack").boot()
 s.setMidiInputDevice(1)
 
-# notes = {'c':0,'c#':1,'d':2,'d#':3,'e':4,'f':5,'f#':6,'g':7,'g#':9,'a':10,'a#':11}
-# def note_name_to_midi(filename):
-#     note = filename.split("/")[1].split('_')[1]
-#     note_name = re.match("[a-g][#b]?", note).group()
-#     pitch = notes[note_name] * note[-1] * 12
+notes = {'c':0,'c#':1,'d':2,'d#':3,'e':4,'f':5,'f#':6,'g':7,'g#':9,'a':10,'a#':11}
+def note_name_to_midi(filename):
+    note = filename.split('_')[1]
+    note_name = re.match("[a-g][#b]?", note).group()
+    pitch = notes[note_name] * note[-1] * 12
 
 sounds = [[] for i in range(200)]
 
 for note_file in listdir("snds"):
     if isfile("snds/" + note_file):
-        sounds[i].append("snds/" + note_file)
+        sounds[note_name_to_midi(note_file)].append("snds/" + note_file)
 
 players = []
 
@@ -28,7 +28,7 @@ def start_new_note(event, note, velocity):
     players.append(player)
     player.out()
 
-midi_in = RawMidi(scale=0)
+midi_in = RawMidi(start_new_note)
 
 s.start()
 s.gui(locals)
